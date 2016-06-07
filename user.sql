fa1fed3a514b42027152fb46131ba14440ced298
Chris Impicciche <peechiz@bakudan.local>
2016-06-07 11:46:39 -0500

    finishes sql ddl

--- user.sql
+++ user.sql
@@ -5,13 +5,13 @@ CREATE TABLE user(
 );
 
 CREATE TABLE subscriptions(
-  user_id REFERENCES user.id,
+  user_id integer REFERENCES user(id),
   product varchar,
   sub_end date
 );
 
 CREATE TABLE avatar(
-  user_id REFERENCES user.id,
+  user_id integer REFERENCES user(id),
   url_big varchar
   url_small varchar
 );
